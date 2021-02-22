a8e3a558c179c8ede3f9a70ee50f82076950966d
Mitra Nami <a.mxnami@gmail.com>
2021-02-21 19:51:41 -0500

    Insert the image and info into the DOM when user clicks on the next button

--- app.js
+++ app.js
@@ -24,3 +24,41 @@ const data = [
     image: 'https://randomuser.me/api/portraits/men/83.jpg'
   }
 ];
+
+// Profile Iterator
+let profile = profileIterator(data);
+
+
+// Event handler for clicking the next button
+document.getElementById('next')
+  .addEventListener('click', nextProfile);
+
+
+function nextProfile(evt) {
+  const {value, done} = profile.next();
+  if (!done) {
+    // Insert the Image
+    document.getElementById('imageDisplay').innerHTML =
+    `<img src=${value.image} alt="avatar" />`;
+    // Insert the User Info
+    document.getElementById('profileDisplay').innerHTML =
+      `<ul class="list-group m-2">
+        <li class="list-group-item">Name: ${value.name}</li>
+        <li class="list-group-item">Age: ${value.age}</li>
+        <li class="list-group-item">Location: ${value.location}</li>
+        <li class="list-group-item">Preference: ${value.gender} Looking for ${value.lookingfor}</li>
+      </ul>`;
+  } 
+}
+
+
+function profileIterator(data) {
+  let index = 0;
+  return {
+    next: function() {
+      return index < data.length ?
+        {value: data[index++], done: false} :
+        {done: true};
+    }
+  }
+}
